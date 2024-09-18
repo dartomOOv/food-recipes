32c329acbfcd91a26897ef61177e2842d9fcdc84
Danyil <dartomov@gmail.com>
2024-09-18 14:42:11 +0200

    Create urls and raw views for login, registration and welcome pages

--- recipes_app/urls.py
+++ recipes_app/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from recipes_app.views import index
+from recipes_app.views import (
+    index,
+    registration,
+    login,
+)
 
 urlpatterns = [
-    path("", index, name="home-page")
+    path("", index, name="welcome-page"),
+    path("registration/", registration, name="registration-page"),
+    path("login", login, name="login-page"),
 ]
 app_name = "recipes"
--- recipes_app/views.py
+++ recipes_app/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "base/home_page.html")
+
+    return render(request, "base/welcome_page.html")
+
+def registration(request: HttpRequest) -> HttpResponse:
+
+    return render(request, "accounts/registration.html")
+
+def login(request: HttpRequest) -> HttpResponse:
+
+    return render(request, "accounts/login.html")
