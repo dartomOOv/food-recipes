2b8bd39814f77e211d39552e381f958061720c04
Danyil <dartomov@gmail.com>
2024-09-19 16:26:28 +0200

    Refactor path 'login/' to work with 'CustomLoginView' class

--- recipes_app/urls.py
+++ recipes_app/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from recipes_app.views import (
     index,
     registration,
-    login,
     main_page,
+    CustomLoginView,
 )
 
 urlpatterns = [
     path("", index, name="welcome-page"),
     path("registration/", registration, name="registration-page"),
-    path("login", login, name="login-page"),
+    path("login", CustomLoginView.as_view(), name="login-page"),
     path("recipes", main_page, name="recipes-list")
 ]
 app_name = "recipes"
