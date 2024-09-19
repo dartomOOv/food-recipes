b99c20704b944340f24e1e7a5067c9f9083aab0b
Danyil <dartomov@gmail.com>
2024-09-19 12:05:40 +0200

    Create tag for managing dish authors representation

---
+++ recipes_app/templatetags/query_join.py
@@ -0,0 +1,9 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def join_authors(user_dishes):
+    return ", ".join(
+        [user_dish.user.username for user_dish in user_dishes.all().prefetch_related("User")]
+    )
