c0274861714b65dd400b4e5848b2eb84dcdcd2bc
Danyil <dartomov@gmail.com>
2024-09-19 13:15:04 +0200

    Refactor view function to show all recipes

--- recipes_app/views.py
+++ recipes_app/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from recipes_app.models import Dish
+
 
 def index(request: HttpRequest) -> HttpResponse:
 
@@ -13,3 +15,12 @@ def registration(request: HttpRequest) -> HttpResponse:
 def login(request: HttpRequest) -> HttpResponse:
 
     return render(request, "accounts/login.html")
+
+def main_page(request: HttpRequest) -> HttpResponse:
+    dishes = Dish.objects.all()
+    context = {
+        "dishes": dishes
+    }
+
+    return render(request, "recipes/recipes_list.html", context=context)
+
