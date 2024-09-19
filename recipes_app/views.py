e3f85393a0e3877da128d5d38190a2bd6f7a8035
Danyil <dartomov@gmail.com>
2024-09-19 16:22:57 +0200

    Remove testing 'login' function and create 'CustomLoginView' class

--- recipes_app/views.py
+++ recipes_app/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.views import LoginView
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
+from django.views import generic
 
+from recipes_app.forms import UserLoginForm
 from recipes_app.models import Dish
 
 
@@ -12,9 +16,15 @@ def registration(request: HttpRequest) -> HttpResponse:
 
     return render(request, "accounts/registration.html")
 
-def login(request: HttpRequest) -> HttpResponse:
 
-    return render(request, "accounts/login.html")
+class CustomLoginView(LoginView):
+    form_class = UserLoginForm
+    template_name = "accounts/login.html"
+    success_url = "recipes/recipes_list.html"
+
+    def get_success_url(self):
+        return reverse("recipes:recipes-list")
+
 
 def main_page(request: HttpRequest) -> HttpResponse:
     dishes = Dish.objects.all()
