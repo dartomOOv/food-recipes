692990535090cc7be276527fbb90540a1003da98
Danyil <dartomov@gmail.com>
2024-09-19 12:52:06 +0200

    Fix method to select_related in join_authors function

--- recipes_app/templatetags/query_join.py
+++ recipes_app/templatetags/query_join.py
@@ -5,5 +5,5 @@ register = template.Library()
 @register.filter
 def join_authors(user_dishes):
     return ", ".join(
-        [user_dish.user.username for user_dish in user_dishes.all().prefetch_related("User")]
+        [user_dish.user.username for user_dish in user_dishes.all().select_related("user")]
     )
