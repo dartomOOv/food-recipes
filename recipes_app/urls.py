f1d20707094c7c87bfb099aa73f9a57e3817cbcc
Danyil <dartomov@gmail.com>
2024-09-19 13:15:40 +0200

    Change imported function name

--- recipes_app/urls.py
+++ recipes_app/urls.py
@@ -3,11 +3,13 @@ from recipes_app.views import (
     index,
     registration,
     login,
+    main_page,
 )
 
 urlpatterns = [
     path("", index, name="welcome-page"),
     path("registration/", registration, name="registration-page"),
     path("login", login, name="login-page"),
+    path("recipes", main_page, name="recipes-list")
 ]
 app_name = "recipes"
