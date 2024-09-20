b61a3ff1677062b5ca350e71bf4e177da98df892
Danyil <dartomov@gmail.com>
2024-09-20 10:05:24 +0200

    Add path for dish detail

--- recipes_app/urls.py
+++ recipes_app/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from recipes_app.views import (
-    index,
     registration,
     main_page,
     CustomLoginView,
+    RecipeDetailView,
 )
 
 urlpatterns = [
     path("registration/", registration, name="registration-page"),
     path("login", CustomLoginView.as_view(), name="login-page"),
-    path("", main_page, name="recipes-list")
+    path("", main_page, name="recipes-list"),
+    path("<slug:slug>/", RecipeDetailView.as_view(), name="recipe-detail"),
 ]
 app_name = "recipes"
