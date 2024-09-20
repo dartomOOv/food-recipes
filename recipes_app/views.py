13b3f8b7b370e17fa17548980357aa52791a86a9
Danyil <dartomov@gmail.com>
2024-09-20 15:23:28 +0200

    Add class view to manage dish saving in SavedUserDish

--- recipes_app/views.py
+++ recipes_app/views.py
@@ -2,12 +2,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
-from django.views import generic
+from django.views import generic, View
 
 from recipes_app.forms import UserLoginForm
-from recipes_app.models import Dish
+from recipes_app.models import Dish, SavedUserDish
 
 
 def index(request: HttpRequest) -> HttpResponse:
@@ -41,3 +41,13 @@ class RecipeDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = "recipes/recipe_detail.html"
     model = Dish
     slug_field = "slug"
+
+class SaveRemoveRecipe(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        dish = get_object_or_404(Dish, slug=kwargs["slug"])
+        saved_dish = SavedUserDish.objects.filter(dish=dish, user=request.user)
+        if saved_dish.exists():
+            saved_dish.delete()
+        else:
+            SavedUserDish.objects.create(dish=dish, user=request.user)
+        return redirect(dish.get_absolute_url())
