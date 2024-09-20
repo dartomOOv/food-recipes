08bbdedbce6cd6d8dfa4f608faea029a6f682efc
Danyil <dartomov@gmail.com>
2024-09-20 21:36:47 +0200

    Join in templatetags 'existence_check' and 'query_join' and rename to 'object_filters'

--- recipes_app/templatetags/object_filters.py
+++ recipes_app/templatetags/object_filters.py
@@ -1,5 +1,7 @@
 from django import template
 
+from recipes_app.models import SavedUserDish
+
 register = template.Library()
 
 @register.filter
@@ -7,3 +9,9 @@ def join_authors(user_dishes):
     return ", ".join(
         [user_dish.user.username for user_dish in user_dishes.all().select_related("user")]
     )
+
+
+@register.filter
+def dish_is_saved(dish, user):
+    filtered = SavedUserDish.objects.filter(dish=dish, user=user)
+    return filtered.exists()
