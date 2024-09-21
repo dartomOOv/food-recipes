87c6d784d01e631503e04ac14f9f0d0cecc9c7da
Danyil <dartomov@gmail.com>
2024-09-21 11:24:23 +0200

    Create new path for user saved recipes

--- recipes_app/urls.py
+++ recipes_app/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from recipes_app.views import (
     RecipeDetailView,
     SaveRemoveRecipe,
-    MainPageView
+    MainPageView,
+    SavedRecipes,
 )
 
 urlpatterns = [
     path("", MainPageView.as_view(), name="recipes-list"),
+    path("saved/", SavedRecipes.as_view(), name="saved_recipes"),
     path("<slug:slug>/", RecipeDetailView.as_view(), name="recipe-detail"),
     path("<slug:slug>/save/", SaveRemoveRecipe.as_view(), name="recipe-save-remove"),
 ]
