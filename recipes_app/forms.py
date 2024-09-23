f595be31b012edfb1d37fc2a1e2a37f00ea8a95b
Danyil <dartomov@gmail.com>
2024-09-23 11:52:31 +0200

    Create dish search form in 'forms.py'

--- recipes_app/forms.py
+++ recipes_app/forms.py
@@ -122,3 +122,17 @@ class DishCreateForm(forms.ModelForm):
         widgets = {
             'ingredients': forms.CheckboxSelectMultiple,  # To display ingredients as checkboxes
         }
+
+
+class DishSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+            "placeholder": "Search by name",
+            'class': 'form-control',
+            }
+        )
+    )
