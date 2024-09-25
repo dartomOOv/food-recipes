bf8fc71f454cdf14184f2834c9d90a175a31cec4
Danyil <dartomov@gmail.com>
2024-09-25 16:32:22 +0200

    Fix debug status

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = os.getenv("DEBUG") != "False"
 
 ALLOWED_HOSTS = [
     "food-recipes-xxy7.onrender.com",
